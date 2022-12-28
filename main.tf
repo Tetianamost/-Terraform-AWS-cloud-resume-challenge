848bb59e9f039fe24f8c746a0a2a38adef751c6d
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-28 12:41:56 -0700

    added custom origin config to origin block

--- main.tf
+++ main.tf
@@ -24,6 +24,12 @@ resource "aws_cloudfront_distribution" "resume_website" {
     s3_origin_config {
       origin_access_identity = "${aws_cloudfront_origin_access_identity.resume_website.cloudfront_access_identity_path}"
     }
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
   }
 
   viewer_certificate {
@@ -31,7 +37,12 @@ resource "aws_cloudfront_distribution" "resume_website" {
     ssl_support_method  = "sni-only"
   }
   enabled = true
-   
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
 
   restrictions {
     geo_restriction {
