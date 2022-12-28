5db1719f14a778f5d849001ed63d8b6a77c66933
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-28 15:17:07 -0700

    enclosed

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ resource "aws_api_gateway_integration" "lambda_integration_get" {
     }
     uri = "${aws_lambda_function.resume_website.invoke_arn}"
   }
-
+}
 resource "aws_api_gateway_integration" "lambda_integration_post" {
   rest_api_id   = "${aws_api_gateway_rest_api.resume_website.id}"
   resource_id   = "${aws_api_gateway_resource.resume_website.id}"
@@ -108,7 +108,7 @@ resource "aws_api_gateway_integration" "lambda_integration_post" {
     }
     uri = "${aws_lambda_function.resume_website.invoke_arn}"
 }
-
+}
 resource "aws_api_gateway_method" "resume_website_get" {
   rest_api_id   = "${aws_api_gateway_rest_api.resume_website.id}"
   resource_id   = "${aws_api_gateway_resource.resume_website.id}"
