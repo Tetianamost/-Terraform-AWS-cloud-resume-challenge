a5bf19f0ad7f94a022db37d4d00fb767cd378b25
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-29 12:04:04 -0700

    corrected conflict

--- main.tf
+++ main.tf
@@ -79,40 +79,21 @@ resource "aws_api_gateway_resource" "resume_website" {
   path_part   = "resume"
 }
 resource "aws_api_gateway_method" "resume_website_get" {
-  rest_api_id             = aws_api_gateway_rest_api.resume_website.id
-  resource_id             = aws_api_gateway_resource.resume_website.id
-  http_method             = "GET"
-  authorization           = "NONE"
-  type                    = "AWS"
-  integration_http_method = "POST"
-  uri                     = aws_api_gateway_integration.resume_website_get.uri
-
-
-
-
+  rest_api_id   = aws_api_gateway_rest_api.resume_website.id
+  resource_id   = aws_api_gateway_resource.resume_website.id
+  http_method   = "GET"
+  authorization = "NONE
 }
 
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.resume_website.id
   resource_id             = aws_api_gateway_resource.resume_website.id
-  http_method             = "GET"
+  http_method             = "aws_api_gateway_method.resume_website_get
   type                    = "AWS_PROXY"
   integration_http_method = "POST"
   uri                     = aws_lambda_function.resume_website.invoke_arn
 
 
-}
-
-
-resource "aws_api_gateway_method_response" "resume_website_get_response" {
-  rest_api_id = aws_api_gateway_rest_api.resume_website.id
-  resource_id = aws_api_gateway_resource.resume_website.id
-  http_method = aws_api_gateway_method.resume_website_get.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = true
-  }
 }
 resource "aws_api_gateway_method" "resume_website_options" {
   rest_api_id   = aws_api_gateway_rest_api.resume_website.id
