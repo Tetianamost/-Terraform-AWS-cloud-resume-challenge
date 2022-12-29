90b3530396a28ccee6072589e63164b08cd51093
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-29 12:07:27 -0700

    http meethod

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ resource "aws_api_gateway_method" "resume_website_get" {
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.resume_website.id
   resource_id             = aws_api_gateway_resource.resume_website.id
-  http_method             = aws_api_gateway_method.resume_website_get
+  http_method             = aws_api_gateway_method.resume_website_get.http_method
   type                    = "AWS_PROXY"
   integration_http_method = "POST"
   uri                     = aws_lambda_function.resume_website.invoke_arn
