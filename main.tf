e31e9653907b5e627b215ccf655c586ba1e6512e
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-29 12:19:50 -0700

    added options integration

--- main.tf
+++ main.tf
@@ -100,7 +100,15 @@ resource "aws_api_gateway_method" "resume_website_options" {
   http_method   = "OPTIONS"
   authorization = "NONE"
 }
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.resume_website.id
+  resource_id             = aws_api_gateway_resource.resume_website.id
+  http_method             = aws_api_gateway_method.resume_website_options.http_method
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = aws_lambda_function.resume_website.invoke_arn
 
+}
 resource "aws_api_gateway_method_response" "resume_website_options" {
   rest_api_id = aws_api_gateway_rest_api.resume_website.id
   resource_id = aws_api_gateway_resource.resume_website.id
