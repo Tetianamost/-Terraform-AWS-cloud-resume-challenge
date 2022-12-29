6585766668abb59e2eb2cff41621de8f1e27ec88
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-28 21:40:31 -0700

    removed defaults

--- variables.tf
+++ variables.tf
@@ -1,14 +1,13 @@
 variable "resume_website_bucket_arn" {
-  default = aws_s3_bucket.resume_website.arn
- 
+
+
 }
 
 variable "api_arn" {
-  default = aws_api_gateway_rest_api.resume_website.execution_arn
- 
+
+
 }
 variable "dynamodb_table" {
-  default = aws_dynamodb_table.name
 }
 
 variable "api_gateway_rest_api" {
