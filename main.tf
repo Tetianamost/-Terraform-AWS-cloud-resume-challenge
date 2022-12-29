7f9756e2a533a585771329a6416ff1cae0409a7a
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-29 10:37:57 -0700

    configured api integration

--- main.tf
+++ main.tf
@@ -81,23 +81,25 @@ resource "aws_api_gateway_resource" "resume_website" {
 resource "aws_api_gateway_integration" "lambda_integration_get" {
   rest_api_id = aws_api_gateway_rest_api.resume_website.id
   resource_id = aws_api_gateway_resource.resume_website.id
-  http_method = "GET"
+  http_method = ${aws_api_gateway_method.resume_website.http_method}"
   type        = "AWS_PROXY"
   request_parameters = {
     "integration.request.header.X-Authorization" = "'static'"
   }
   uri = aws_lambda_function.resume_website.invoke_arn
+  integration_http_method = "GET"
 }
 
 resource "aws_api_gateway_integration" "lambda_integration_post" {
   rest_api_id = aws_api_gateway_rest_api.resume_website.id
   resource_id = aws_api_gateway_resource.resume_website.id
-  http_method = "POST"
+  http_method = ${aws_api_gateway_method.resume_website.http_method}"
   type        = "AWS_PROXY"
   request_parameters = {
     "integration.request.header.X-Authorization" = "'static'"
   }
   uri = aws_lambda_function.resume_website.invoke_arn
+  integration_http_method = "POST"
 }
 
 resource "aws_api_gateway_method" "resume_website_get" {
