94f71d1c52d7fb0c4c8a6e6c6ef821d340557b48
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-28 22:35:47 -0700

    added option method

--- main.tf
+++ main.tf
@@ -123,21 +123,26 @@ resource "aws_api_gateway_method_response" "resume_website_get_response" {
     "method.response.header.Access-Control-Allow-Origin" = true
   }
 }
+resource "aws_api_gateway_method" "resume_website_options" {
+  rest_api_id   = aws_api_gateway_rest_api.resume_website.id
+  resource_id   = aws_api_gateway_resource.resume_website.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
 
 
 resource "aws_api_gateway_method_response" "resume_website_options" {
   rest_api_id = aws_api_gateway_rest_api.resume_website.id
   resource_id = aws_api_gateway_resource.resume_website.id
-  http_method = "OPTIONS"
+  http_method = aws_api_gateway_method.resume_website_options.http_method
   status_code = "200"
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers"  = true,
     "method.response.header.Access-Control-Allow-Methods"  = true,
     "method.response.header.Access-Control-Allow-Origin"   = true,
-    "method.response.header.Access-Control-Expose-Headers" = true
-  }
-}
+    "method.response.header.Access-Control
+
 
 
 # Create the Lambda function
