cdfc7d8b1b248efe6b8aceada460cd1f996a3b89
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-28 21:34:33 -0700

    updated vars file

--- variables.tf
+++ variables.tf
@@ -1,16 +1,15 @@
 variable "resume_website_bucket_arn" {
   default = aws_s3_bucket.resume_website.arn
-  type    = string
+ 
 }
 
 variable "api_arn" {
-  default = "aws_api_gateway_rest_api.resume_website.execution_arn"
-  type    = string
+  default = aws_api_gateway_rest_api.resume_website.execution_arn
+ 
 }
 variable "dynamodb_table" {
-  default = "my-resume-website-table"
+  default = aws_dynamodb_table.name
 }
 
 variable "api_gateway_rest_api" {
-  default = "my-resume-website-api"
 }
