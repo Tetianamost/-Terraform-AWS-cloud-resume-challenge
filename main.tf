40cc9777c56441e71e19b3ced641e2825d90c2c8
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-29 11:05:57 -0700

    removed post api method

--- main.tf
+++ main.tf
@@ -84,12 +84,7 @@ resource "aws_api_gateway_method" "resume_website_get" {
   http_method   = "GET"
   authorization = "NONE"
 }
-resource "aws_api_gateway_method" "resume_website_post" {
-  rest_api_id   = aws_api_gateway_rest_api.resume_website.id
-  resource_id   = aws_api_gateway_resource.resume_website.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
+
 resource "aws_api_gateway_integration" "lambda_integration_get" {
   rest_api_id = aws_api_gateway_rest_api.resume_website.id
   resource_id = aws_api_gateway_resource.resume_website.id
@@ -100,18 +95,6 @@ resource "aws_api_gateway_integration" "lambda_integration_get" {
   }
   uri                     = aws_lambda_function.resume_website.invoke_arn
   integration_http_method = "GET"
-}
-
-resource "aws_api_gateway_integration" "lambda_integration_post" {
-  rest_api_id = aws_api_gateway_rest_api.resume_website.id
-  resource_id = aws_api_gateway_resource.resume_website.id
-  http_method = aws_api_gateway_method.resume_website_post.http_method
-  type        = "AWS_PROXY"
-  request_parameters = {
-    "integration.request.header.X-Authorization" = "'static'"
-  }
-  uri                     = aws_lambda_function.resume_website.invoke_arn
-  integration_http_method = "POST"
 }
 
 
@@ -146,19 +129,19 @@ resource "aws_api_gateway_method_response" "resume_website_options" {
     "method.response.header.Access-Control-Expose-Headers" = true
   }
 }
-resource "aws_api_gateway_deployment" "resume_website" {
+resource "aws_api_gateway_deployment" "resume_website_get" {
   rest_api_id = aws_api_gateway_rest_api.resume_website.id
   stage_name  = "prod"
   depends_on = [
-        aws_api_gateway_method.resume_website_get,
-        aws_api_gateway_integration.lambda_integration_get
-      ]
+    aws_api_gateway_method.resume_website_get,
+    aws_api_gateway_integration.lambda_integration_get
+  ]
 }
 
 resource "aws_api_gateway_stage" "resume_website" {
   rest_api_id   = aws_api_gateway_rest_api.resume_website.id
   stage_name    = "prod"
-  deployment_id = aws_api_gateway_deployment.resume_website.id
+  deployment_id = aws_api_gateway_deployment.resume_website_get.id
 }
 
 output "api_endpoint_url" {
