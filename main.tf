694ba3c651efe7d20ea76f6d774db37da22580e8
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-29 12:20:25 -0700

    options

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ resource "aws_api_gateway_method" "resume_website_options" {
   http_method   = "OPTIONS"
   authorization = "NONE"
 }
-resource "aws_api_gateway_integration" "lambda_integration" {
+resource "aws_api_gateway_integration" "lambda_integration_options" {
   rest_api_id             = aws_api_gateway_rest_api.resume_website.id
   resource_id             = aws_api_gateway_resource.resume_website.id
   http_method             = aws_api_gateway_method.resume_website_options.http_method
