f878c1884f7a91a7d8b7c9f5630a15c443078763
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-29 10:52:42 -0700

    aded api stage

--- main.tf
+++ main.tf
@@ -146,8 +146,13 @@ resource "aws_api_gateway_method_response" "resume_website_options" {
     "method.response.header.Access-Control-Expose-Headers" = true
   }
 }
+resource "aws_api_gateway_stage" "resume_website" {
+  rest_api_id = aws_api_gateway_rest_api.resume_website.id
+  stage_name  = "prod"
+}
+
 output "api_endpoint_url" {
-  value = aws_api_gateway_rest_api.resume_website.invoke_url
+  value = aws_api_gateway_stage.resume_website.invoke_url
 }
 
 
