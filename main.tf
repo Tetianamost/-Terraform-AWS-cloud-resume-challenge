e604d233f6aaa1a964499127e9bc2797d3348b6e
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-29 11:46:05 -0700

    removed int

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "aws_api_gateway_resource" "resume_website" {
 resource "aws_api_gateway_method" "resume_website_get" {
   rest_api_id             = aws_api_gateway_rest_api.resume_website.id
   resource_id             = aws_api_gateway_resource.resume_website.id
-  http_method             = aws_api_gateway_method.resume_website_get.http_method
+  http_method             = "GET"
   authorization           = "NONE"
  
 }
