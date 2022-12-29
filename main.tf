4e9f898d51e767c1b423022589f02ca1519c0cb6
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-29 11:42:05 -0700

    -

--- main.tf
+++ main.tf
@@ -79,16 +79,14 @@ resource "aws_api_gateway_resource" "resume_website" {
   path_part   = "resume"
 }
 resource "aws_api_gateway_method" "resume_website_get" {
-  rest_api_id   = aws_api_gateway_rest_api.resume_website.id
-  resource_id   = aws_api_gateway_resource.resume_website.id
-  http_method   = "GET"
-  authorization = "NONE"
-    
-    type                    = "AWS_PROXY"
-    integration_http_method = "POST"
-    uri                     = aws_lambda_function.resume_website.invoke_arn
-    passthrough_behavior    = "WHEN_NO_MATCH"
-  
+  rest_api_id             = aws_api_gateway_rest_api.resume_website.id
+  resource_id             = aws_api_gateway_resource.resume_website.id
+  http_method             = aws_api_gateway_method.resume_website_get.http_method
+  authorization           = "NONE"
+  type                    = "AWS_PROXY"
+  integration_http_method = "GET"
+  uri                     = aws_lambda_function.resume_website.invoke_arn
+
 }
 
 resource "aws_api_gateway_integration" "lambda_integration" {
