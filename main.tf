b4a4644ed0b23851939466213ce3c2c0bcf804b0
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-29 10:41:18 -0700

    formatted

--- main.tf
+++ main.tf
@@ -89,28 +89,28 @@ resource "aws_api_gateway_method" "resume_website_post" {
   resource_id   = aws_api_gateway_resource.resume_website.id
   http_method   = "POST"
   authorization = "NONE"
-  }
+}
 resource "aws_api_gateway_integration" "lambda_integration_get" {
   rest_api_id = aws_api_gateway_rest_api.resume_website.id
   resource_id = aws_api_gateway_resource.resume_website.id
-  http_method = ${aws_api_gateway_method.resume_website.get.http_method}"
+  http_method = aws_api_gateway_method.resume_website.get.http_method
   type        = "AWS_PROXY"
   request_parameters = {
     "integration.request.header.X-Authorization" = "'static'"
   }
-  uri = aws_lambda_function.resume_website.invoke_arn
+  uri                     = aws_lambda_function.resume_website.invoke_arn
   integration_http_method = "GET"
 }
 
 resource "aws_api_gateway_integration" "lambda_integration_post" {
   rest_api_id = aws_api_gateway_rest_api.resume_website.id
   resource_id = aws_api_gateway_resource.resume_website.id
-  http_method = ${aws_api_gateway_method.resume_website.post.http_method}"
+  http_method = aws_api_gateway_method.resume_website.post.http_method
   type        = "AWS_PROXY"
   request_parameters = {
     "integration.request.header.X-Authorization" = "'static'"
   }
-  uri = aws_lambda_function.resume_website.invoke_arn
+  uri                     = aws_lambda_function.resume_website.invoke_arn
   integration_http_method = "POST"
 }
 
