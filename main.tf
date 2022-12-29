ecd3955187d53a0f902e119d4fca6a155620c0b8
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-29 11:26:10 -0700

    fixed misspell

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ resource "aws_api_gateway_method" "resume_website_get" {
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "lambda_integrationt" {
+resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.resume_website.id
   resource_id             = aws_api_gateway_resource.resume_website.id
   http_method             = aws_api_gateway_method.resume_website_get.http_method
