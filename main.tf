d7b8ee24457be6f29eed686e9509f13f6d93b81c
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-29 12:33:12 -0700

    removed auth int

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   resource_id             = aws_api_gateway_resource.resume_website.id
   http_method             = aws_api_gateway_method.resume_website_get.http_method
   type                    = "AWS_PROXY"
-  authorization = "NONE"
+
   integration_http_method = "POST"
   uri                     = aws_lambda_function.resume_website.invoke_arn
 
@@ -106,7 +106,7 @@ resource "aws_api_gateway_integration" "lambda_integration_options" {
   resource_id             = aws_api_gateway_resource.resume_website.id
   http_method             = aws_api_gateway_method.resume_website_options.http_method
   type                    = "AWS_PROXY"
-  authorization = "NONE"
+  
   integration_http_method = "POST"
   uri                     = aws_lambda_function.resume_website.invoke_arn
 
