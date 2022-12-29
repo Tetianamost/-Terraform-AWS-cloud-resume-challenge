07c1a5186ef62b3cb4a53db531f7ff35e82b46e0
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-28 21:30:35 -0700

    removed quotes

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "resume_website_bucket_arn" {
-  default = "aws_s3_bucket.resume_website.arn"
-  type = "string"
+  default = aws_s3_bucket.resume_website.arn
+  type    = string
 }
 
 variable "api_arn" {
-    default = "aws_api_gateway_rest_api.resume_website.execution_arn"
-    type = string
+  default = "aws_api_gateway_rest_api.resume_website.execution_arn"
+  type    = string
 }
 variable "dynamodb_table" {
-     default = "my-resume-website-table"
+  default = "my-resume-website-table"
 }
 
 variable "api_gateway_rest_api" {
-    default = "my-resume-website-api"
+  default = "my-resume-website-api"
 }
