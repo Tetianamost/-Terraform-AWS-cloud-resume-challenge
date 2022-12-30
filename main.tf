0dcf6884f5154ccaf8e7acceb97da1ac1d764b93
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-30 14:17:47 -0700

    added missing "

--- main.tf
+++ main.tf
@@ -182,7 +182,7 @@ resource "aws_lambda_permission" "allow_execute" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.resume_website.arn
   principal     = "apigateway.amazonaws.com"
-  source_arn    = ${aws_api_gateway_rest_api.resume_website.execution_arn}/*"
+  source_arn    = "${aws_api_gateway_rest_api.resume_website.execution_arn}/*"
 }
 
 
