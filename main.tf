93548aa7a2bc363962eabdd62d59b541ff1d0ef5
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-30 12:40:00 -0700

    removed passthrough

--- main.tf
+++ main.tf
@@ -118,7 +118,6 @@ resource "aws_api_gateway_integration_response" "lambda_integration_response" {
   status_code             = aws_api_gateway_method_response.resume_website_get.status_code
   integration_http_method = "ANY"
   uri                     = aws_lambda_function.resume_website.invoke_arn
-  passthrough_behavior    = "WHEN_NO_MATCH"
 }
 resource "aws_api_gateway_method" "resume_website_options" {
   rest_api_id   = aws_api_gateway_rest_api.resume_website.id
@@ -134,7 +133,7 @@ resource "aws_api_gateway_integration" "lambda_integration_options" {
 
   integration_http_method = "ANY"
   uri                     = aws_lambda_function.resume_website.invoke_arn
-  passthrough_behavior    = "WHEN_NO_MATCH"
+
 }
 resource "aws_api_gateway_method_response" "resume_website_options" {
   rest_api_id = aws_api_gateway_rest_api.resume_website.id
