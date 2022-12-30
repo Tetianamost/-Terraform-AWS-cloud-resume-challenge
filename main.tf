040afab34f29701aecb8f4c660dcfd88bcec4d1a
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-30 11:44:34 -0700

    set http method to any

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ resource "aws_api_gateway_resource" "resume_website" {
 resource "aws_api_gateway_method" "resume_website_get" {
   rest_api_id      = aws_api_gateway_rest_api.resume_website.id
   resource_id      = aws_api_gateway_resource.resume_website.id
-  http_method      = "GET"
+  http_method      = "ANY"
   authorization    = "NONE"
   api_key_required = false
 }
