e111d5bdb37db2ebac4dbc7422ab876582211fe7
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-12-30 12:32:18 -0700

    reverted back to aws proxy type

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.resume_website.id
   resource_id             = aws_api_gateway_resource.resume_website.id
   http_method             = aws_api_gateway_method.resume_website_get.http_method
-  type                    = "AWS_LAMBDA"
+  type                    = "AWS_PROXY"
   integration_http_method = "ANY"
   uri                     = aws_lambda_function.resume_website.invoke_arn
   passthrough_behavior    = "WHEN_NO_MATCH"
@@ -130,7 +130,7 @@ resource "aws_api_gateway_integration" "lambda_integration_options" {
   rest_api_id = aws_api_gateway_rest_api.resume_website.id
   resource_id = aws_api_gateway_resource.resume_website.id
   http_method = aws_api_gateway_method.resume_website_options.http_method
-  type        = "AWS_LAMBDA"
+  type        = "AWS_PROXY"
 
   integration_http_method = "ANY"
   uri                     = aws_lambda_function.resume_website.invoke_arn
