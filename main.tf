eeb5b60035bd6f16f75ea5e5a26303784b25f1e0
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2023-01-02 17:47:25 -0700

    removed patch method

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "resume_website" {
   }
   cors_rule {
     allowed_headers = ["*"]
-    allowed_methods = ["GET", "PUT", "POST", "DELETE", "HEAD", "PATCH"]
+    allowed_methods = ["GET", "PUT", "POST", "DELETE", "HEAD"]
     allowed_origins = ["*"]
     expose_headers  = ["ETag"]
     max_age_seconds = 0
