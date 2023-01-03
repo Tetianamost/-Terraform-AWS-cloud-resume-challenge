514d8461670344d2616671d72cd57c0123eb104d
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2023-01-03 11:16:27 -0700

    reverted back naming

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ resource "aws_acm_certificate" "resume_website" {
 }
 # Create the Lambda function
 resource "aws_lambda_function" "resume_website" {
-  s3_bucket     = "my-resume-web-lambda"
+  s3_bucket     = "my-resume-website-lambda"
   s3_key        = "lambda.zip"
   function_name = "handler"
   handler       = "index.handler"
@@ -96,7 +96,7 @@ resource "aws_lambda_function" "resume_website" {
 }
 
 resource "aws_s3_bucket_policy" "lambda_access" {
-  bucket = "my-resume-web-lambda"
+  bucket = "my-resume-website-lambda"
 
   policy = <<EOF
 {
@@ -110,8 +110,8 @@ resource "aws_s3_bucket_policy" "lambda_access" {
       },
       "Action": "s3:*",
       "Resource": [
-        "arn:aws:s3:::my-resume-web-lambda",
-        "arn:aws:s3:::my-resume-web-lambda/*"
+        "arn:aws:s3:::my-resume-website-lambda",
+        "arn:aws:s3:::my-resume-website-lambda/*"
       ]
     }
   ]
@@ -129,7 +129,7 @@ resource "aws_lambda_permission" "allow_s3_access" {
 
 
 resource "aws_iam_role" "lambda_role" {
-  name               = "my-resume-web-lambda-role"
+  name               = "my-resume-website-lambda-role"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -150,7 +150,7 @@ EOF
 
 
 resource "aws_iam_policy" "lambda_dynamodb_policy" {
-  name   = "my-resume-web-lambda-dynamodb-policy"
+  name   = "my-resume-websitesite-lambda-dynamodb-policy"
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -224,16 +224,16 @@ resource "aws_iam_role_policy_attachment" "api_gateway_access" {
   policy_arn = aws_iam_policy.api_gateway_access.arn
 }
 
-resource "aws_api_gateway_rest_api" "resume-web" {
+resource "aws_api_gateway_rest_api" "resume-website" {
 
   name = "API for my resume website"
 
 }
 
 
-resource "aws_api_gateway_method" "resume-web" {
-  rest_api_id   = aws_api_gateway_rest_api.resume-web.id
-  resource_id   = aws_api_gateway_rest_api.resume-web.root_resource_id
+resource "aws_api_gateway_method" "resume-website" {
+  rest_api_id   = aws_api_gateway_rest_api.resume-website.id
+  resource_id   = aws_api_gateway_rest_api.resume-website.root_resource_id
   http_method   = "ANY"
   authorization = "NONE"
 
@@ -241,10 +241,10 @@ resource "aws_api_gateway_method" "resume-web" {
 
 
 
-resource "aws_api_gateway_integration" "resume-web" {
-  rest_api_id = aws_api_gateway_rest_api.resume-web.id
-  resource_id = aws_api_gateway_rest_api.resume-web.root_resource_id
-  http_method = aws_api_gateway_method.resume-web.http_method
+resource "aws_api_gateway_integration" "resume-website" {
+  rest_api_id = aws_api_gateway_rest_api.resume-website.id
+  resource_id = aws_api_gateway_rest_api.resume-website.root_resource_id
+  http_method = aws_api_gateway_method.resume-website.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -264,14 +264,14 @@ resource "aws_lambda_permission" "apigw" {
   function_name = aws_lambda_function.resume_website.arn
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.resume-web.id}/*/*"
+  source_arn = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.resume-website.id}/*/*"
 }
-resource "aws_api_gateway_method_response" "resume-web" {
+resource "aws_api_gateway_method_response" "resume-website" {
 
-  depends_on      = [aws_api_gateway_method.resume-web]
-  rest_api_id     = aws_api_gateway_rest_api.resume-web.id
-  resource_id     = aws_api_gateway_rest_api.resume-web.root_resource_id
-  http_method     = aws_api_gateway_method.resume-web.http_method
+  depends_on      = [aws_api_gateway_method.resume-website]
+  rest_api_id     = aws_api_gateway_rest_api.resume-website.id
+  resource_id     = aws_api_gateway_rest_api.resume-website.root_resource_id
+  http_method     = aws_api_gateway_method.resume-website.http_method
   status_code     = 200
   response_models = { "application/json" = "Empty" }
   response_parameters = {
@@ -282,10 +282,10 @@ resource "aws_api_gateway_method_response" "resume-web" {
   }
 }
 
-resource "aws_api_gateway_integration_response" "resume-web" {
-  rest_api_id = aws_api_gateway_rest_api.resume-web.id
-  resource_id = aws_api_gateway_rest_api.resume-web.root_resource_id
-  http_method = aws_api_gateway_method.resume-web.http_method
+resource "aws_api_gateway_integration_response" "resume-website" {
+  rest_api_id = aws_api_gateway_rest_api.resume-website.id
+  resource_id = aws_api_gateway_rest_api.resume-website.root_resource_id
+  http_method = aws_api_gateway_method.resume-website.http_method
   status_code = "200"
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'*'",
@@ -295,9 +295,9 @@ resource "aws_api_gateway_integration_response" "resume-web" {
   }
 }
 
-resource "aws_api_gateway_deployment" "resume-web" {
-  depends_on  = [aws_api_gateway_integration_response.resume-web, aws_api_gateway_integration.resume-web]
-  rest_api_id = aws_api_gateway_rest_api.resume-web.id
+resource "aws_api_gateway_deployment" "resume-website" {
+  depends_on  = [aws_api_gateway_integration_response.resume-website, aws_api_gateway_integration.resume-website]
+  rest_api_id = aws_api_gateway_rest_api.resume-website.id
   stage_name  = "dev1"
 }
 
@@ -317,7 +317,7 @@ resource "aws_route53_record" "resume_website" {
 }
 
 resource "aws_dynamodb_table" "resume_website" {
-  name           = "my-resume-web-table"
+  name           = "my-resume-website-table"
   billing_mode   = "PROVISIONED"
   write_capacity = 5
   read_capacity  = 5
