701a5d0c0557e731d675a6d7c021cdb7757d05d2
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2023-01-02 18:37:10 -0700

    added ,

--- main.tf
+++ main.tf
@@ -291,7 +291,7 @@ resource "aws_api_gateway_integration_response" "api_root" {
   http_method = aws_api_gateway_method.api_root.http_method
   status_code = "200"
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'*'"
+    "method.response.header.Access-Control-Allow-Headers" = "'*'",
     "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS,GET,PUT,PATCH,DELETE'",
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
 
